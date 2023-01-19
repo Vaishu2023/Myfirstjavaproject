37559d0b47be34b80a18ab64c9c0956300a227e1
prash <prash@192.168.0.42>
2023-01-19 11:48:45 -0500

    Uploading my first project

---
+++ src/display/Print.java
@@ -0,0 +1,14 @@
+package display;
+
+public class Print {
+
+	public static void main(String[] args) {
+		System.out.println("Wlcome to my world");
+		System.out.println("My name is Vaishali");
+				System.out.println("I love music");
+				System.out.println("20x30= ");
+				System.out.println(20*30);
+
+	}
+
+}
